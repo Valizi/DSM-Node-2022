189d2582d4ddfc3a6f94f411f424a81a7f551a34
Eduardo Valizi <duduvalizi16@gmail.com>
2022-04-29 21:13:38 -0300

    29/04

---
+++ livros-webii/backend/src/config/databases.js
@@ -0,0 +1,11 @@
+module.exports = {
+    host: 'localhost',
+    dialect : 'mysql',
+    username:'root',
+    password: "fatec123*",
+    databases: "webii",
+    define:{
+        timestamps: true,
+        underscored: true,
+    }
+};

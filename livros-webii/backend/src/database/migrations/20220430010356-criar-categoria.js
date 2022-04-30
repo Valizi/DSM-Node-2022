fd0461ad53d816db7615b295ca236d2569228f1a
Eduardo Valizi <duduvalizi16@gmail.com>
2022-04-29 22:13:56 -0300

    29/04

---
+++ livros-webii/backend/src/database/migrations/20220430010356-criar-categoria.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  up:async  (queryInterface, Sequelize)=> {
+    return await queryInterface.createTable('categorias',{
+      id:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey:true;
+      },
+
+      cat_descricao:{
+        type:Sequelize.STRING(30),
+        allowNull: false,
+
+      },
+      
+
+      create_at:{
+        type:Sequelize.DATE,
+        allowNull: false,
+
+      },
+      update_at:{
+        type:Sequelize.DATE,
+        allowNull: false,
+
+      },
+    });
+  },
+
+  down: async (queryInterface,Sequelize)=> {
+    return queryInterface.dropTable('categorias')
+  }
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
